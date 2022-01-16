00b55b41485d62bc78d93e2e150413a567408620
roremDev <rorem.94.developer@gmail.com>
2022-01-15 21:18:45 -0600

    perf: ⚡️ changed validators definition

--- src/validators/request.validator.ts
+++ src/validators/request.validator.ts
@@ -1,47 +1,36 @@
 import Joi from 'joi'
-
 /**
  * @description validation catalog schemas
  * @typedef {object} Validators
- * @property {object} params - params request
- * @property {object} query - query request
- * @property {object} body - body request
+ * @property {object} comment - example of comment schema validator
  */
-interface Validators {
-    params: Joi.ObjectSchema
-    query: Joi.ObjectSchema
-}
-const validators: Validators = {
-    params: Joi.object({
-        keyword: Joi.string(),
-    }),
-    query: Joi.object({
-        type: Joi.array().items(
-            Joi.string()
-                .valid('Breakfast', 'Lunch', 'Dinner')
-                .message('type must be a valid option')
-        ),
-        status: Joi.array().items(
-            Joi.string()
-                .valid('Draft', 'Done', 'Published', 'Archived')
-                .message('status must be a valid option')
-        ),
-        category: Joi.array().items(
-            Joi.string()
-                .valid('Flexetarian', 'Vegetarian', 'Low Carb')
-                .message('category must be a valid option')
-        ),
+const validators: { [key: string]: Joi.ObjectSchema } = {
+    comment: Joi.object({
+        userId: Joi.string()
+            .regex(/^[1-9]+[0-9]*$/)
+            .message('⚠ ID must be a valid number'),
+        message: Joi.string().min(1).max(150).messages({
+            'string.base': '⚠ MESSAGE must be a string',
+            'string.empty': '⚠ MESSAGE cannot be empty',
+            'string.min': '⚠ MESSAGE cannot be void',
+            'string.max': '⚠ MESSAGE must be at most 150 characters',
+        }),
+        tags: Joi.string()
+            .valid('Software', 'Design', 'Soft Skills')
+            .messages({
+                'any.only': `⚠ TAGS must be a valid option:
+                    - Flexetarian
+                    - Vegetarian
+                    - Low Carb
+                `,
+            }),
     }),
 }
 /**
- * @description validate schema from params
+ * @description validate schema request
  * @param {string} schema - schema selection
  * @param {object} args - object to validate
  * @returns {object} - joi validation object
  */
-export const validate = (
-    schema: string,
-    args: Object
-): Joi.ValidationResult => {
-    return validators[schema as keyof Validators].validate(args)
-}
+export const validate = (schema: string, args: Object): Joi.ValidationResult =>
+    validators[schema].validate(args)
